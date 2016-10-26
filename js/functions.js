7a67d413a2b5be66bcddfcc141bcdd8154eabf98
chhrahul <chhabra.odesk.mca@gmail.com>
2016-10-26 15:41:47 +0530

    4

--- js/functions.js
+++ js/functions.js
@@ -1744,11 +1744,13 @@ function loadgamification() {
                 // var iframeSrcUrlTest = "http://ektalehar.com/projects/oce/links.html";
                 // var iframeSrcUrlMike = "https://experience.live/g-homepage/-/OCintranet-100041/?i=%2FOC-Heroes%2FYoutube-ContentID";
 
-                $(".welcome-container").html('<iframe mozallowfullscreen="true" webkitallowfullscreen="true" allowfullscreen="true" src=' + iframeSrcUrl + ' name="homepage-content" id="homepage-content" />');
+                // $(".welcome-container").html('<iframe mozallowfullscreen="true" webkitallowfullscreen="true" allowfullscreen="true" src=' + iframeSrcUrl + ' name="homepage-content" id="homepage-content" />');
 
-                var thisIframesHeight = window.parent.$("#homepage-content").height();
-                var welHeight = parseInt(thisIframesHeight) + parseInt(40);                 
-                $(".weltempdiv").html("<style> .welcome-container { height: " + welHeight + "px !important; position: absolute !important; margin: -3px 0 0 !important; padding: 0; overflow: scroll !important; display: inline-flex !important; } #homepage-content { padding-top: 0 !important; padding-bottom: 138px !important; overflow: scroll !important; } </style>");
+                // var thisIframesHeight = window.parent.$("#homepage-content").height();
+                // var welHeight = parseInt(thisIframesHeight) + parseInt(40);                 
+                // $(".weltempdiv").html("<style> .welcome-container { height: " + welHeight + "px !important; position: absolute !important; margin: -3px 0 0 !important; padding: 0; overflow: scroll !important; display: inline-flex !important; } #homepage-content { padding-top: 0 !important; padding-bottom: 138px !important; overflow: scroll !important; } </style>");
+                $("#homepage-content").show();
+                $('.welcome-container').html("");
 
                 //alert(len)
                 if(checkdefined(localStorage.menu) == 'yes')
