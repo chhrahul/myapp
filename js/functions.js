ffcd73ff43893354b71343db8f7bd235d953f36d
chhrahul <chhabra.odesk.mca@gmail.com>
2016-10-27 12:01:51 +0530

    6

--- js/functions.js
+++ js/functions.js
@@ -1746,7 +1746,7 @@ function loadgamification() {
                 $(".welcome-container").html('<iframe mozallowfullscreen="true" webkitallowfullscreen="true" allowfullscreen="true" src=' + iframeSrcUrl + ' name="homepage-content" id="homepage-content" />');
 
                 var thisIframesHeight = window.parent.$("#homepage-content").height();
-                 
+
                 if (thisIframesHeight > 550) {
                 	var welHeight = (parseInt(thisIframesHeight) + parseInt(10)) + "px";  
                 	var welHeight = thisIframesHeight + "px";  
@@ -1754,7 +1754,7 @@ function loadgamification() {
                 else {
                 	var welHeight = "550px";
                 }           
-                $(".weltempdiv").html("<style> .welcome-container { height: " + welHeight + "px !important; position: absolute !important; margin: -3px 0 0 !important; padding: 0; overflow: scroll !important; display: inline-flex !important; } #homepage-content { padding-top: 0 !important; padding-bottom: 138px !important; overflow: scroll !important; } </style>");
+                $(".weltempdiv").html("<style> .welcome-container { height: " + welHeight + "px !important; position: fixed !important; margin: -3px 0 0 !important; padding: 0; overflow: scroll !important; display: inline-flex !important; } #homepage-content { padding-top: 0 !important; padding-bottom: 138px !important; overflow: scroll !important; } </style>");
                 // $("#homepage-content").attr("src", iframeSrcUrl);
                 // $("#homepage-content").show().css("height", welHeight);
                 // $('.welcome-container').html("");
