33e1d4ec722267df34a2f0055fb52248014aba45
chhrahul <chhabra.odesk.mca@gmail.com>
2016-10-27 11:37:25 +0530

    6

--- js/functions.js
+++ js/functions.js
@@ -1743,24 +1743,21 @@ function loadgamification() {
 
                 var iframeSrcUrlTest = "http://ektalehar.com/projects/oce/links.html";
                
-                // $(".welcome-container").html('<iframe mozallowfullscreen="true" webkitallowfullscreen="true" allowfullscreen="true" src=' + iframeSrcUrl + ' name="homepage-content" id="homepage-content" />');
+                $(".welcome-container").html('<iframe mozallowfullscreen="true" webkitallowfullscreen="true" allowfullscreen="true" src=' + iframeSrcUrl + ' name="homepage-content" id="homepage-content" />');
 
                 var thisIframesHeight = window.parent.$("#homepage-content").height();
-                // var welHeight = (parseInt(thisIframesHeight) + parseInt(10)) + "px";  
+                 
                 if (thisIframesHeight > 550) {
                 	var welHeight = (parseInt(thisIframesHeight) + parseInt(10)) + "px";  
                 	var welHeight = thisIframesHeight + "px";  
-                	// console.log("if");
                 }   
                 else {
                 	var welHeight = "550px";
-                	// console.log("else");
-                }
-                console.log(thisIframesHeight + " , " + welHeight);            
-                // $(".weltempdiv").html("<style> .welcome-container { height: " + welHeight + "px !important; position: absolute !important; margin: -3px 0 0 !important; padding: 0; overflow: scroll !important; display: inline-flex !important; } #homepage-content { padding-top: 0 !important; padding-bottom: 138px !important; overflow: scroll !important; } </style>");
-                $("#homepage-content").attr("src", iframeSrcUrl);
-                $("#homepage-content").show().css("height", welHeight);
-                $('.welcome-container').html("");
+                }           
+                $(".weltempdiv").html("<style> .welcome-container { height: " + welHeight + "px !important; position: absolute !important; margin: -3px 0 0 !important; padding: 0; overflow: scroll !important; display: inline-flex !important; } #homepage-content { padding-top: 0 !important; padding-bottom: 138px !important; overflow: scroll !important; } </style>");
+                // $("#homepage-content").attr("src", iframeSrcUrl);
+                // $("#homepage-content").show().css("height", welHeight);
+                // $('.welcome-container').html("");
 
                 //alert(len)
                 if(checkdefined(localStorage.menu) == 'yes')
