5c78ddfce0b061fce22c1facad93395c011bd7f5
chhrahul <chhabra.odesk.mca@gmail.com>
2016-11-18 16:12:16 +0530

    11

--- js/functions.js
+++ js/functions.js
@@ -1756,7 +1756,7 @@ function loadgamification() {
                 $(".weltempdiv").html("<style> .welcome-container { height: " + welHeight + "px !important; margin: -3px 0 0 !important; padding: 0; overflow: scroll !important; display: inline-flex !important; } #homepage-content { padding-top: 0 !important; padding-bottom: 138px !important; overflow: scroll !important; } </style>");
 				// $(".weltempdiv").html("<style> .welcome-container { height: " + welHeight + "px !important; position: fixed !important; margin: -3px 0 0 !important; padding: 0; overflow: scroll !important; display: inline-flex !important; } #homepage-content { padding-top: 0 !important; padding-bottom: 138px !important; overflow: scroll !important; } </style>");
                 // $("#homepage-content").attr("src", iframeSrcUrl);
-                $("#homepage-content").show().css("height", welHeight);
+                $("#homepage-content").css("height", welHeight);
                 // $('.welcome-container').html("");
 
                 //alert(len)
