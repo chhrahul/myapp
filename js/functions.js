1fd968bcacba8014bfc8912f755ed56a8117eff3
chhrahul <chhabra.odesk.mca@gmail.com>
2016-11-18 16:37:16 +0530

    11

--- js/functions.js
+++ js/functions.js
@@ -1744,7 +1744,7 @@ function loadgamification() {
                
                 $(".welcome-container").html('<iframe mozallowfullscreen="true" webkitallowfullscreen="true" allowfullscreen="true" src=' + iframeSrcUrl + ' name="homepage-content" id="homepage-content" />');
 
-                // var thisIframesHeight = window.parent.$("#homepage-content").height();
+                var thisIframesHeight = window.parent.$("#homepage-content").height();
 
                 if (thisIframesHeight > 550) {
                 	var welHeight = (parseInt(thisIframesHeight) + parseInt(10)) + "px";  
@@ -1753,7 +1753,7 @@ function loadgamification() {
                 else {
                 	var welHeight = "550px";
                 }           
-                $(".weltempdiv").html("<style> .welcome-container { height: " + welHeight + "px !important; margin: -3px 0 0 !important; padding: 0; overflow: scroll !important; display: inline-flex !important; } #homepage-content { padding-top: 0 !important; padding-bottom: 138px !important; overflow: scroll !important; } </style>");
+                // $(".weltempdiv").html("<style> .welcome-container { height: " + welHeight + " !important; margin: -3px 0 0 !important; padding: 0; overflow: scroll !important; display: inline-flex !important; } #homepage-content { padding-top: 0 !important; padding-bottom: 138px !important; overflow: scroll !important; } </style>");
 				// $(".weltempdiv").html("<style> .welcome-container { height: " + welHeight + "px !important; position: fixed !important; margin: -3px 0 0 !important; padding: 0; overflow: scroll !important; display: inline-flex !important; } #homepage-content { padding-top: 0 !important; padding-bottom: 138px !important; overflow: scroll !important; } </style>");
                 // $("#homepage-content").attr("src", iframeSrcUrl);
                 $("#homepage-content").css("height", welHeight);
